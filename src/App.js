15bedbc630d10e3a245a2438b681a772a461c4bc
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-07 14:36:25 -0500

    initial app file  setup
    installed and set up tailwind etc

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first React App</h1>
+      <h2>My first React App</h2>
+      <h3>My first React App</h3>
+      <h4>My first React App</h4>
+      <h5>My first React App</h5>
+      <h6>My first React App</h6>
+    </div>
+  );
+}
+
+
+export default App;
