09285ca904319a904abd4f6fa921517f8a15f2d9
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-09 18:17:22 -0500

    Update OAuth.jsx
    configured the google auth

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
 import { FcGoogle } from 'react-icons/fc'
+import { toast } from 'react-toastify';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
+import { db } from '../firebase';
+import { useNavigate } from 'react-router-dom';
 
 export default function OAuth() {
+    const navigate = useNavigate();
+
+    async function googleAuth () {
+
+      try {
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider(); 
+        const result = await signInWithPopup(auth, provider); 
+        const user = result.user;
+
+        // check for user
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if(!docSnap.exists()) {
+          await setDoc(docRef, {
+            name: user.displayName,
+            email: user.email,
+            timestamp: serverTimestamp(),
+          });
+        }
+
+        navigate('/');
+
+      } catch (error) {
+        toast.error("Could not authenticate with Google");
+      }
+    }
+
     return (
         <button
+          onClick={googleAuth}
           type="button"
             className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
         >
