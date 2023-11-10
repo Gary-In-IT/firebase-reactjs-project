8e7ff7cdaf1f060f47db6d7f6ad00813818d78bb
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-10 09:06:55 -0500

    wrapped up sign-in page
    adjusted toast a bit too

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
 
       <ToastContainer
         position="bottom-center"
-        autoClose={5000}
+        autoClose={1500}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
