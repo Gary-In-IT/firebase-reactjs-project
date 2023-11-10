286b09f8f0ffff0751a593203c8660b52f3434ce
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-10 16:13:16 -0500

    user update to firestore
    making sure the db is connected and updating.

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,55 +1,112 @@
-import { useState } from "react"
-import { getAuth } from 'firebase/auth';
+import { useState } from "react";
+import { getAuth, updateProfile } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
+import { toast } from "react-toastify";
+import { doc, updateDoc } from 'firebase/firestore';
+import { db } from './../firebase';
 
 export default function Profile() {
-
   const auth = getAuth();
   const navigate = useNavigate();
+  const [changeProfile, setChangeProfile] = useState(false);
 
-  const [formData, setFormData] = useState ( {
-    name: auth.currentUser.displayName, email: auth.currentUser.email
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
   });
 
   const { name, email } = formData;
 
-  function onLogout () {
+  function onLogout() {
     auth.signOut();
-    navigate("/")
+    navigate("/");
   }
 
-  return (
+  function onChange(e) {
+    setFormData ((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
 
-    <>
+  // update Firestore
+  async function onSubmit() {
+
+    try {
+      if(auth.currentUser.displayName !== name) {
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        })        
+      }
+      toast.success("Profile updated")
+
+    } catch (error) {
+      toast.error("Error updating profile")
+    }
+
+  }
     
+
+  return (
+    <>
       <section className="flex justify-center items-center text-center flex-col  max-w-6xl mx-auto ">
-      
-        <h1 className="mb-6 text=3xl text-center mt-6 font-bold">My Awesome Profile</h1>
+        <h1 className="mb-6 text=3xl text-center mt-6 font-bold">
+          My Awesome Profile
+        </h1>
 
         <div className="w-full md:w-[50%] mt-6 px-3">
-
           <form>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              disabled={!changeProfile}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out ${
+                changeProfile && "bg-red-200 focus:bg-red-200"
+              }`}
+            />
 
-          <input type="text" id="name" value={name} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              disabled
+              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"
+            />
 
-          <input type="email" id="email" value={email} disabled className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition ease-in-out"/>
+            <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
+              <p className="flex items-center">
+                Need to change your name?{" "}
+                <span
+                  onClick={() => {
+                    changeProfile && onSubmit()
+                    setChangeProfile((prevState) => !prevState);
+                  }}  
+                  
 
-          <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg mb-6">
-            <p className="flex items-center">Need to change your name? <span className="text-red-500 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span></p>
 
-            <p onClick={onLogout} className="text-blue-500 hover:text-blue-700 transition ease-in-out duration-200 ml-1 cursor-pointer  ">Sign Out</p>
-              
-          </div>
+                  className="text-red-500 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeProfile ? "Apply Change" : "Edit"}
+                </span>
+              </p>
 
+              <p
+                onClick={onLogout}
+                className="text-blue-500 hover:text-blue-700 transition ease-in-out duration-200 ml-1 cursor-pointer  "
+              >
+                Sign Out
+              </p>
+            </div>
           </form>
-
         </div>
-
       </section>
-
     </>
-
-
-  )
+  );
 }
