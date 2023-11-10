54d70b65c03fa5b440c885421f103762fe7aa9c3
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-10 17:24:21 -0500

    auth state change for log in
    setting user state for private routes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,24 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-
-
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 export default function Header() {
 
-  // const [pageState, setPageState] = useState("Sign in");
+  const [pageState, setPageState] = useState("Sign in");
 
   const location = useLocation();
   const navigate = useNavigate();
+
+  const auth = getAuth();
+  useEffect (() =>{
+    onAuthStateChanged(auth, (user)=>{
+      if (user) {
+        setPageState("Profile");
+      }else {
+        setPageState("Sign in");
+      }
+    });
+  }, [auth]);
 
   function pathMatchRoute(route) {
     if (route === location.pathname) {
@@ -50,11 +60,11 @@ export default function Header() {
 
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMatchRoute("/sign-in") && "!text-black !border-b-red-500"
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) && "!text-black !border-b-red-500"
               }`}
-              onClick={() => navigate("/sign-in")}
+              onClick={() => navigate("/profile")}
             >
-              Sign In
+              {pageState}
             </li>
           </ul>
         </div>
