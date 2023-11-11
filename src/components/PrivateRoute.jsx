d57ecbbcecb6b37adbfc650c8c00104d8be0e31f
Gary-FullStack <114956248+Gary-FullStack@users.noreply.github.com>
2023-11-11 12:49:48 -0500

    some UX stuff added
    spinner loader and what not

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,13 +1,16 @@
 import { Outlet, Navigate } from "react-router-dom";
 import {useAuthStatus} from "../hooks/useAuthStatus";
+import Spinner from "./Spinner";
 
 
 export default function PrivateRoute() {
 
   const { loggedIn, checkingStatus } = useAuthStatus();
 
-  if (checkingStatus)
-    return <div>Checking authentication status...</div>;
+  if (checkingStatus) {
+    return <Spinner />;
+  }
+  
   return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
 
 }
